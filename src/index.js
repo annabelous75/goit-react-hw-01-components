77dbf65d4970a9f389fbc87ff6e83ca80c673281
annabelous75 <57856008+annabelous75@users.noreply.github.com>
2023-02-08 14:06:59 +0200

    summ

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './components/App';
-import { theme } from 'utils/theme';
-import { ThemeProvider } from '@emotion/react';
+import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
--- src/utils/theme.js
+++
@@ -1,7 +0,0 @@
-export const theme = {
-  colors: {
-    heading: 'rgb(245, 159, 159)',
-    secondary: 'white',
-    wrapper: 'rgb(217, 213, 213, 0.5)',
-  },
-};
